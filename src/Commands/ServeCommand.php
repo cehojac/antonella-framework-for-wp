e0ba0dd525fac88ce32f8238328829e2295897f4
Carlos Herrera <cehojac@gmail.com>
2021-04-18 23:09:33 +0200

    [FIX] mejorandi un poquito

--- src/Commands/ServeCommand.php
+++ src/Commands/ServeCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use InvalidArgumentException;
 
 /**
  *	@see https://symfony.com/doc/current/console.html
@@ -37,6 +38,7 @@ class ServeCommand extends BaseCommand {
 	
 	protected $destiny = '';
 	
+	
 	protected function configure()
     {
         $this->setDescription('Create a server local')
@@ -55,12 +57,14 @@ class ServeCommand extends BaseCommand {
 		// obtenemos las options desde consola
 		$force = $input->getOption('force');
 		
+		
 		if (!file_exists('.env')) {
             $output->writeln("<error>Antonella response: You need create and config the .env file. you have a .env-example file reference</error>");
             die();
         }
 		
 		$dotenv = Dotenv::create($dir);
+		
         $dotenv->load();
         if (!getenv('DBNAME')) {
             $output->writeln("<error>Antonella response: You need config the DBNAME into .env file</error>");
@@ -71,10 +75,9 @@ class ServeCommand extends BaseCommand {
         $dbuser = getenv('DBUSER');
         $dbpass = getenv('DBPASS');
         		
-		$testdir = getenv('TEST_DIR') ? getenv('TEST_DIR') : $this->testdir;
-        $port = getenv('PORT') ? getenv('PORT') : $this->port;
-		$locale = getenv('LOCALE') ? getenv('LOCALE') : $this->locale;
-		
+		$testdir = getenv('TEST_DIR',$this->testdir);
+        $port = getenv('PORT',$this->port);
+		$locale = getenv('LOCALE',$this->locale);
 		$slash = DIRECTORY_SEPARATOR;
         $pluginname = basename($dir);
         $filename = basename($dir).'.zip';
@@ -101,7 +104,7 @@ class ServeCommand extends BaseCommand {
 		// si le pasamo el port y force como argumentos
 		if ($force && $input->getOption('port')) {
 			// seteamos el nuevo port
-			$port = $input->getOption('port');
+			$port = $input->getOption('port')?$input->getOption('port'):$port;
 		}
 		
 		system("php wp-cli.phar config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --path=$testdir  --extra-php=\" define( 'WP_DEBUG', true ); define( 'WP_DEBUG_LOG', true ); define( 'QM_ENABLE_CAPS_PANEL', true );  \" $extra_php");
@@ -110,8 +113,9 @@ class ServeCommand extends BaseCommand {
 		
 		if ($force) {
 			// setea siteurl y homeurl
-			system("php wp-cli.phar option update home http://localhost:$port --path=$testdir");
-			system("php wp-cli.phar option update siteurl http://localhost:$port --path=$testdir");
+			system("php wp-cli.phar option update home http://localhost:$port --path=$testdir --quiet");
+			system("php wp-cli.phar option update siteurl http://localhost:$port --path=$testdir --quiet");
+			
 		}
 		
 		// comprimimos nuetros plugin
@@ -169,11 +173,11 @@ class ServeCommand extends BaseCommand {
         $zip->close();
         file_exists($this->destiny) ? unlink($this->destiny) : false;
 		
-		system("php wp-cli.phar plugin activate $pluginname --path=$testdir");
+		system("php wp-cli.phar plugin activate $pluginname --path=$testdir --quiet");
         $output->writeln("Your plugin has been refreshed in test.");
 		
 		foreach ($plugins as $plugin)		
-			system("php wp-cli.phar plugin install $plugin --path=$testdir --activate");
+			system("php wp-cli.phar plugin install $plugin --path=$testdir --activate --quiet");
         
 		
 	}
